46e33d2189a9c5edc9b448832872a8bab1c7b24c
DevDivyansh <576divyanshsharma@gmail.com>
2026-05-22 19:44:13 +0530

    chore: add import in packages

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 
+import {createUserSchema} from "@monorepo/utils";
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-
 
 app.get('/', (req, res) => {
   res.json({message : "Hello world!!!"});
